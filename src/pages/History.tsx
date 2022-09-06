261dae6e1a15de736ceb8e7565dc8b5653ef5992
Simlex <gettosimlex@gmail.com>
2022-09-06 01:21:43 +0100

    Updated page

---
+++ src/pages/History.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent, ReactElement } from "react";
+
+interface HistoryProps {
+    
+}
+ 
+const History: FunctionComponent<HistoryProps> = ():ReactElement => {
+    return ( 
+        <>History</>
+     );
+}
+ 
+export default History;
