0c5f3ea8c6231a045062f440053156435af90450
Simlex <simlexafol@gmail.com>
2022-09-06 17:55:58 +0100

    Added module style and updated toolbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,20 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+// import './Home.css';
+import style from './Home.module.scss';
+import img from '../../public/assets/shapes.svg';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ecommerce App</IonTitle>
+        <IonToolbar class={style.ionToolbar} color='primary'>
+          <div className={style.container}>
+            <IonTitle class={style.iontitle}>
+              <img src='../../public/assets/shapes.svg' />
+            </IonTitle>
+            <div className={style.title}>Home</div>
+          </div>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
