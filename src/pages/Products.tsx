ec61565f49794e6e9a0338fa5f87d24835d4cd72
Simlex <simlexafol@gmail.com>
2022-09-06 17:56:21 +0100

    Created products page

---
+++ src/pages/Products.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent, ReactElement } from "react";
+
+interface ProductsProps {
+    
+}
+ 
+const Products: FunctionComponent<ProductsProps> = ():ReactElement => {
+    return ( 
+        <>Products page</>
+     );
+}
+ 
+export default Products;
